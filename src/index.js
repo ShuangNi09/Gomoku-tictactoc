e43e15605f1dd624bd468d44205555b9abbae1e8
bob <shuangstella09@berkeley.edu>
2019-01-11 17:12:31 +0800

    style

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+function Squar(props) {
+  return (
+    <button className="squar" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -20,127 +28,136 @@ class Board extends React.Component {
     );
   }
 
+  renderSquar(i) {
+    return (
+      <Squar
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
   render() {
     return (
       <div>
         <div className="board-row">
           {this.renderSquare(0)}
-          {this.renderSquare(1)}
+          {this.renderSquar(1)}
           {this.renderSquare(2)}
-          {this.renderSquare(3)}
+          {this.renderSquar(3)}
           {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquar(5)}
           {this.renderSquare(6)}
-          {this.renderSquare(7)}
+          {this.renderSquar(7)}
           {this.renderSquare(8)}
-          {this.renderSquare(9)}
+          {this.renderSquar(9)}
         </div>
         <div className="board-row">
-          {this.renderSquare(10)}
+          {this.renderSquar(10)}
           {this.renderSquare(11)}
-          {this.renderSquare(12)}
+          {this.renderSquar(12)}
           {this.renderSquare(13)}
-          {this.renderSquare(14)}
+          {this.renderSquar(14)}
           {this.renderSquare(15)}
-          {this.renderSquare(16)}
+          {this.renderSquar(16)}
           {this.renderSquare(17)}
-          {this.renderSquare(18)}
+          {this.renderSquar(18)}
           {this.renderSquare(19)}
         </div>
         <div className="board-row">
           {this.renderSquare(20)}
-          {this.renderSquare(21)}
+          {this.renderSquar(21)}
           {this.renderSquare(22)}
-          {this.renderSquare(23)}
+          {this.renderSquar(23)}
           {this.renderSquare(24)}
-          {this.renderSquare(25)}
+          {this.renderSquar(25)}
           {this.renderSquare(26)}
-          {this.renderSquare(27)}
+          {this.renderSquar(27)}
           {this.renderSquare(28)}
-          {this.renderSquare(29)}
+          {this.renderSquar(29)}
         </div>
         <div className="board-row">
-          {this.renderSquare(30)}
+          {this.renderSquar(30)}
           {this.renderSquare(31)}
-          {this.renderSquare(32)}
+          {this.renderSquar(32)}
           {this.renderSquare(33)}
-          {this.renderSquare(34)}
+          {this.renderSquar(34)}
           {this.renderSquare(35)}
-          {this.renderSquare(36)}
+          {this.renderSquar(36)}
           {this.renderSquare(37)}
-          {this.renderSquare(38)}
+          {this.renderSquar(38)}
           {this.renderSquare(39)}
         </div>
         <div className="board-row">
           {this.renderSquare(40)}
-          {this.renderSquare(41)}
+          {this.renderSquar(41)}
           {this.renderSquare(42)}
-          {this.renderSquare(43)}
+          {this.renderSquar(43)}
           {this.renderSquare(44)}
-          {this.renderSquare(45)}
+          {this.renderSquar(45)}
           {this.renderSquare(46)}
-          {this.renderSquare(47)}
+          {this.renderSquar(47)}
           {this.renderSquare(48)}
-          {this.renderSquare(49)}
+          {this.renderSquar(49)}
         </div>
         <div className="board-row">
-          {this.renderSquare(50)}
+          {this.renderSquar(50)}
           {this.renderSquare(51)}
-          {this.renderSquare(52)}
+          {this.renderSquar(52)}
           {this.renderSquare(53)}
-          {this.renderSquare(54)}
+          {this.renderSquar(54)}
           {this.renderSquare(55)}
-          {this.renderSquare(56)}
+          {this.renderSquar(56)}
           {this.renderSquare(57)}
-          {this.renderSquare(58)}
+          {this.renderSquar(58)}
           {this.renderSquare(59)}
         </div>
         <div className="board-row">
           {this.renderSquare(60)}
-          {this.renderSquare(61)}
+          {this.renderSquar(61)}
           {this.renderSquare(62)}
-          {this.renderSquare(63)}
+          {this.renderSquar(63)}
           {this.renderSquare(64)}
-          {this.renderSquare(65)}
+          {this.renderSquar(65)}
           {this.renderSquare(66)}
-          {this.renderSquare(67)}
+          {this.renderSquar(67)}
           {this.renderSquare(68)}
-          {this.renderSquare(69)}
+          {this.renderSquar(69)}
         </div>
         <div className="board-row">
-          {this.renderSquare(70)}
+          {this.renderSquar(70)}
           {this.renderSquare(71)}
-          {this.renderSquare(72)}
+          {this.renderSquar(72)}
           {this.renderSquare(73)}
-          {this.renderSquare(74)}
+          {this.renderSquar(74)}
           {this.renderSquare(75)}
-          {this.renderSquare(76)}
+          {this.renderSquar(76)}
           {this.renderSquare(77)}
-          {this.renderSquare(78)}
+          {this.renderSquar(78)}
           {this.renderSquare(79)}
         </div>
         <div className="board-row">
           {this.renderSquare(80)}
-          {this.renderSquare(81)}
+          {this.renderSquar(81)}
           {this.renderSquare(82)}
-          {this.renderSquare(83)}
+          {this.renderSquar(83)}
           {this.renderSquare(84)}
-          {this.renderSquare(85)}
+          {this.renderSquar(85)}
           {this.renderSquare(86)}
-          {this.renderSquare(87)}
+          {this.renderSquar(87)}
           {this.renderSquare(88)}
-          {this.renderSquare(89)}
+          {this.renderSquar(89)}
         </div>
         <div className="board-row">
-          {this.renderSquare(90)}
+          {this.renderSquar(90)}
           {this.renderSquare(91)}
-          {this.renderSquare(92)}
+          {this.renderSquar(92)}
           {this.renderSquare(93)}
-          {this.renderSquare(94)}
+          {this.renderSquar(94)}
           {this.renderSquare(95)}
-          {this.renderSquare(96)}
+          {this.renderSquar(96)}
           {this.renderSquare(97)}
-          {this.renderSquare(98)}
+          {this.renderSquar(98)}
           {this.renderSquare(99)}
         </div>
       </div>
@@ -197,7 +214,9 @@ class Game extends React.Component {
       const desc = move ? "Go to move #" + move : "Go to game start";
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button color="#fff6c2" onClick={() => this.jumpTo(move)}>
+            {desc}
+          </button>
         </li>
       );
     });
